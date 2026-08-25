07576e99e5cfed9cd72be11ccc4c5a0f47b97e14
reddy-saab <kattaanish14376@gmail.com>
2026-08-25 18:33:47 +0530

    Added login, its verification,logout, refresh api and their logic

--- authentication/src/controller/auth.controller.js
+++ authentication/src/controller/auth.controller.js
@@ -5,6 +5,7 @@ const otpModel=require("../model/otp.model")
 const bcrypt=require("bcrypt")
 const {sendEmail}=require("../services/services.mail")
 const {generateOtp, getOtpHtml}=require("../services/utils")
+require("dotenv").config();
 
 async function register(req,res){
   const {username, email,password}= req.body;
@@ -162,4 +163,254 @@ async function login(req,res){
     });
   }
 
-module.exports={register,verifyEmail,login}
+
+async function verifyLoginOtp(req,res){
+      const { email, otp } = req.body;
+
+    const user = await usermodel.findOne({ email });
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User not found"
+        });
+    }
+
+    const otpRecord = await otpModel
+        .findOne({ email, user: user._id,purpose: "login" })
+
+    if (!otpRecord) {
+        return res.status(400).json({
+            message: "OTP not found or expired"
+        });
+    }
+
+      const isValid = await bcrypt.compare(
+    otp,
+    otpRecord.otpHash
+);
+
+ if(!isValid){
+    return res.status(400).json({
+      meaage:"Invalid OTP"
+    })
+  }
+
+    // OTP is correct
+    await otpModel.deleteOne({
+        _id: otpRecord._id
+    });
+
+    const session = new sessionModel({
+  user: user._id,
+  ip:req.ip,
+    userAgent: req.headers["user-agent"]
+});
+
+      // Generate refresh token
+  const refreshToken = jwt.sign(
+    { id: user._id, sessionId:session._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+  session.refreshTokenHash=hashedRefreshToken;
+  await session.save();
+
+  
+  // Generate access token
+  const accessToken = jwt.sign(
+    { id: user._id , sessionId: session._id},
+   process.env.JWT_SECRET,
+    { expiresIn: "10min" }
+  );
+
+  // Set refresh token in cookie
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+  });
+
+  return res.status(200).json({
+    message: "Logged in successful",
+    user:{
+      username:user.username,
+      email:user.email
+    },
+    accessToken
+  });
+
+
+}
+
+async function refreshToken(req,res){
+  const refreshToken=req.cookies.refreshToken; //see in postman cookie section
+  if(!refreshToken){
+    return res.status(401).json({
+      message:"Refresh token not found"
+    })
+  }
+  let decoded;
+
+  try {
+    decoded = jwt.verify(
+      refreshToken,
+      process.env.JWT_SECRET
+    );
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid or expired refresh token"
+    });
+  }
+    // Find the session
+  const session = await sessionModel.findOne({
+    _id: decoded.sessionId,
+    user: decoded.id,
+    revoked: false
+  });
+
+  if (!session) {
+    return res.status(401).json({
+      message: "Invalid refresh token"
+    });
+  }
+
+  const isValid = await bcrypt.compare(
+    refreshToken,
+    session.refreshTokenHash
+  );
+
+    if (!isValid) {
+    return res.status(401).json({
+      message: "Invalid refresh token"
+    });
+  }
+
+  const accessToken=jwt.sign({
+    id: decoded.id
+     },process.env.JWT_SECRET, //JWT Secret   
+     { expiresIn: "15m"} //token expiry
+    )
+
+    //refresh token is also generated again, because if user is using refresh token for a long time, then it will expire, so we need to generate new refresh token also
+ const newRefreshToken= jwt.sign({
+    id:decoded.id,
+     sessionId:decoded.sessionId },
+    process.env.JWT_SECRET,
+    {expiresIn:"7d"}
+
+  )
+
+   // Hash new refresh token
+  const hashedNewRefreshToken = await bcrypt.hash(
+    newRefreshToken,
+    12
+  );
+  // Replace old hash with new hash
+  session.refreshTokenHash = hashedNewRefreshToken;
+
+  await session.save();
+
+
+
+  res.cookie("refreshToken", newRefreshToken, {
+    httpOnly: true, //this cookie cannot be accessed by client side js, only server can access it
+    secure: true,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
+  })
+
+  res.status(200).json({
+    message:"Token refreshed",
+    token: accessToken
+  })
+}
+
+
+async function getme(req,res){
+
+  const token=req.headers.authorization?.split(" ")[1] 
+
+  if(!token){
+    return res.status(401).json({
+      message:"Token not found"
+    })
+  }
+
+  let decoded;
+  try {decoded= jwt.verify(token,process.env.JWT_SECRET)
+  }
+  catch(err){
+    return res.status(401).json({
+      message: err.name
+    })
+  }
+
+   const user=await usermodel.findById(decoded.id)
+ 
+   res.status(200).json({
+    message:"fetched details",
+    username:user.username,
+    email:user.email
+   })
+}
+
+async function logout(req,res){
+  const refreshToken=req.cookies.refreshToken; 
+  if(!refreshToken){
+    return res.status(401).json({
+      message:"Refresh token not found"
+    })
+  }
+
+   let decoded;
+
+  try {
+    decoded = jwt.verify(
+      refreshToken,
+      process.env.JWT_SECRET
+    );
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid or expired refresh token"
+    });
+  }
+
+  // Find the session using session ID
+  const session = await sessionModel.findOne({
+    _id: decoded.sessionId,
+    user: decoded.id,
+    revoked: false
+  });
+
+  if (!session) {
+    return res.status(401).json({
+      message: "Session not found or already revoked"
+    });
+  }
+
+  // Compare refresh token with bcrypt hash
+  const isValid = await bcrypt.compare(
+    refreshToken,
+    session.refreshTokenHash
+  );
+
+  if (!isValid) {
+    return res.status(401).json({
+      message: "Invalid refresh token"
+    });
+  }
+
+  session.revoked=true;
+  await session.save(); //this will update the session in db, so that user cannot use that refresh token again to get new access token
+
+  res.clearCookie("refreshToken") //this will clear the cookie from client side
+
+  res.status(200).json({
+    message:"Logged out successfully"
+  })
+}
+
+module.exports={register,verifyEmail,login,refreshToken, verifyLoginOtp, getme,logout}
--- authentication/src/routes/auth.routes.js
+++ authentication/src/routes/auth.routes.js
@@ -1,13 +1,16 @@
 const express=require('express');
 const authrouter=express.Router();
 
-const {register,verifyEmail,login}=require('../controller/auth.controller')
+const {register,verifyEmail,login,verifyLoginOtp,refreshToken,getme, logout}=require('../controller/auth.controller')
 
 
 
 authrouter.post("/register",register) 
 authrouter.post("/verify-email",verifyEmail)
 authrouter.post("/login",login)
-
+authrouter.post("/verify-login",verifyLoginOtp)
+authrouter.get("/getme",getme)
+authrouter.post("/refresh-token",refreshToken)
+authrouter.post("/logout",logout)
 
 module.exports=authrouter;
